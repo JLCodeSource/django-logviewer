6fa5ee9f8a66071c54e56dd3fef9ef5cd3e493ed
JLCodeSource <JLCodeSource@gmail.com>
2022-01-18 18:03:32 +0000

    Set to defaultrouter

--- viewsite/logviewer/urls.py
+++ viewsite/logviewer/urls.py
@@ -1,47 +1,16 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from logviewer import views
 
-asset_list = views.AssetViewSet.as_view(
-    {
-        "get": "list",
-        "post": "create",
-    }
-)
-
-asset_detail = views.AssetViewSet.as_view(
-    {
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy",
-    }
-)
-
-log_list = views.LogViewSet.as_view(
-    {
-        "get": "list",
-        "post": "create",
-    }
-)
-
-log_detail = views.LogViewSet.as_view(
-    {
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy",
-    }
-)
+router = DefaultRouter()
+router.register(r"assets", views.AssetViewSet)
+router.register(r"logs", views.LogViewSet)
 
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("assets/", asset_list, name="asset-list"),
-    path("assets/<int:pk>/", asset_detail, name="asset-detail"),
-    path("logs/", log_list, name="log-list"),
-    path("logs/<int:pk>/", log_detail, name="log-detail"),
-    path("api-auth/", include("rest_framework.urls")),
+    path("", include(router.urls)),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
